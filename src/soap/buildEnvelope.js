d136a3a2a1590465ead1304fe6f6097f704041ae
Phellpzcg <157931848+Phellpzcg@users.noreply.github.com>
2025-12-30 21:17:40 -0300

    Add buildSoapEnvelope function for SOAP requests

---
+++ src/soap/buildEnvelope.js
@@ -0,0 +1,16 @@
+export function buildSoapEnvelope(gtin14) {
+  return `<?xml version="1.0" encoding="UTF-8"?>
+<soap12:Envelope xmlns:soap12="http://www.w3.org/2003/05/soap-envelope">
+  <soap12:Body>
+    <ccgConsGTIN xmlns="http://www.portalfiscal.inf.br/nfe/wsdl/ccgConsGTIN">
+      <nfeDadosMsg xmlns="http://www.portalfiscal.inf.br/nfe">
+        <![CDATA[
+<consGTIN xmlns="http://www.portalfiscal.inf.br/nfe" versao="1.00">
+  <GTIN>${gtin14}</GTIN>
+</consGTIN>
+        ]]>
+      </nfeDadosMsg>
+    </ccgConsGTIN>
+  </soap12:Body>
+</soap12:Envelope>`;
+}
